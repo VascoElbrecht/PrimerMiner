e70db0881285ff99682a90944cb69536a039be56
Vasco Elbrecht <luckylion07@googlemail.com>
2016-09-28 14:37:16 +0200

    improved example files / tutorial

--- Sample_Data/package_tutorial.R
+++ Sample_Data/package_tutorial.R
@@ -1,7 +1,7 @@
 # Feel free to contact Vasco Elbrecht if you run into issues (twitter: @luckylionde). Enjoy!
 
-
-setwd("~/Desktop/PrimerMiner-master 4") # set the path to the PrimerMinder folder you just downloaded
+# set the path to the PrimerMinder folder you just downloaded
+setwd("~/Desktop/PrimerMiner-master 5")
 
 # install the PrimerMiner package icl dependencies
 install.packages("PrimerMiner", repos = NULL, type="source", dependencies=T)
@@ -9,32 +9,48 @@ install.packages("PrimerMiner", repos = NULL, type="source", dependencies=T)
 # load the package into R
 library("PrimerMiner")
 
+# Set path to sample data
 setwd("Sample_Data")
 
 # creating configuration file and batch downloading reads
 batch_config("config.txt")
 
+# batch download and process sequence data
 batch_download("taxa_small.csv", "config.txt")
 
-##############
-#Now you have to create alignments from the consensus OTU files! Please see manuals on github for more infos (not available jet!)
-##############
+#You have to generate and manually check the alignment! Only extract the region amplified by the primers (+2 * the primer length on each side). We recommend Geneious for doing this!
+
+# You find 5 sample alignments generated with Geneious in the folder "1 COI alignments (unprocessed)". We will use these in the next steps of this tutorial.
+
+# get alignments to process
+fastafiles <- list.files("1 COI alignments (unprocessed)", full.names=T)
+
+# define name and folder to save files in
+fastafiles_export <- paste("2 COI alignments (processed)", list.files("1 COI alignments (unprocessed)"), sep="/")
+
+# process files! This function will remove gaps from the alignments and and apply selective trimming to the primer rgions
+for (i in 1:length(fastafiles)){
+selectivetrim(fastafiles[i], fastafiles_export[i], trimL=25, trimR=26, gaps=0.10, minsequL=100)
+}
 
 
+# The processed gap free alignemts can now be plotted with PrimerMiner or processed with third party primer development software
+# Make sure to chekc that all alignemts have the same length (are aligned)
+alignments <- list.files("2 COI alignments (processed)", full.name=T) # find files
 
-# plot all alignments for manual primer finding!
-alignments <- list.files("1 COi alignments", full.name=T) # find files
-
-pdf("primer_plot_complete.pdf", height=6, width=100)
-plot_alignments(alignments, Order_names=gsub(".*ts/.._(.*)_.*", "\\1", alignments))
+pdf("plot_alignments.pdf", height=4, width=100)
+plot_alignments(alignments, Order_names=gsub(".*./._(.*)_.*", "\\1", alignments))
 dev.off()
+
+
 
 
 # in silico primer evaluation
 
-evaluate_primer("1 COi alignments/01_Plecoptera_folmer.fasta", "GCYCCHGAYATRGCHTTYCC", 218, 237, save="save_evaluation_table.csv", mm_position ="primer_scoring/Position_v1.csv", adjacent=2, mm_type="primer_scoring/Type_v1.csv") 
+evaluate_primer("primer_scoring/01_Plecoptera_subset.fasta", "GGTCAACAAATCATAAAGATATTGG", 1, 25, save="save_evaluation_table_LCO.csv", mm_position ="primer_scoring/Position_v1.csv", adjacent=2, mm_type="primer_scoring/Type_v1.csv") 
 
 
+evaluate_primer("primer_scoring/01_Plecoptera_subset.fasta", "ARYATDGTRATDGCHCCDGC", 585, 604, save="save_evaluation_table_BR1.csv", mm_position ="primer_scoring/Position_v1.csv", adjacent=2, mm_type="primer_scoring/Type_v1.csv", forward=F) 
 
 
 
